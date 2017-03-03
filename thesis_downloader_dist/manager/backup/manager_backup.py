613308436a06da12b592a6e2b105a61839950476
johnsonyue <johnsonyuehit@163.com>
2017-03-03 09:46:26 +0000

    20170303

--- thesis_downloader_dist/manager/backup/manager_backup.py
+++ thesis_downloader_dist/manager/backup/manager_backup.py
@@ -1,7 +1,30 @@
 import os
 import time
+import caida
 
-#time utils.
+def get_all_pid():
+	return [ i for i in os.listdir('/proc') if i.isdigit()];
+
+def get_all_fd(file_path):
+	all_fd = [];
+	for pid in get_all_pid():
+		fd_dir = '/proc/{pid}/fd'.format(pid = pid);
+       		if os.access(fd_dir, os.R_OK) == False:
+			continue;
+
+		for fd in os.listdir(fd_dir):
+			fd_path = os.path.join(fd_dir, fd);
+			if os.path.exists(fd_path) and os.readlink(fd_path) == file_path:
+				all_fd.append(fd_path);
+
+        return all_fd;
+
+def is_occupied(file_name):
+	file_path = os.path.join(os.getcwd() ,file_name);
+	fd_num = len(get_all_fd(file_path));
+	
+	return fd_num >= 1;
+
 def is_heap_year(year):
 	if (not year % 400 or not year % 4):
 		return True;
@@ -95,7 +118,7 @@ def update_state_file(file_name, end_time, start_time="", is_init = False):
 
 	fp.close();
 
-def auth_node(secret_file, node_id, node_key): #primitive, will be replaced
+def auth_node(secret_file, node_id, node_key):
 	fp = open(secret_file,'r');
 	for line in fp.readlines():
 		list = line.split(' ');
@@ -108,8 +131,8 @@ def auth_node(secret_file, node_id, node_key): #primitive, will be replaced
 			
 #enum state={finished, unassigned, pending, terminated};
 def change_state(file_name, date, state):
-	#while(is_occupied(file_name)):
-	#		time.sleep(random.randint(1,3));
+	while(is_occupied(file_name)):
+			time.sleep(random.randint(1,3));
 	fp = open(file_name, 'r');
 	lines = fp.readlines();
 	fp.close();
@@ -125,8 +148,8 @@ def change_state(file_name, date, state):
 	fp.close();
 
 def get_task(file_name):
-	#while(is_occupied(file_name)):
-	#		time.sleep(random.randint(1,3));
+	while(is_occupied(file_name)):
+			time.sleep(random.randint(1,3));
 	fp = open(file_name, 'r');
 	lines = fp.readlines();
 	res = "";
@@ -139,8 +162,8 @@ def get_task(file_name):
 	return res;
 
 def on_notify(log_file_name, state_file_name, type, args):
-	#while(is_occupied(log_file_name)):
-	#		time.sleep(random.randint(1,3));
+	while(is_occupied(log_file_name)):
+			time.sleep(random.randint(1,3));
 	fp = open(log_file_name, 'a');
 	strftime = time.strftime("%Y-%m-%d %H:%M:%S");
 	str = "";
@@ -151,24 +174,30 @@ def on_notify(log_file_name, state_file_name, type, args):
 		time_used = args["time_used"];
 
 		change_state(state_file_name, task, "finished");
-		str = "%s %s %s finished, time used: %s(s)\n" % (strftime, node_id, task, time_used) 
-		fp.write(str)
+		fp.write(strftime + " " + node_id + " " + task + " finished, time used:  " + time_used + "(s)\n");
+		str = strftime + " " + node_id + " " + task + " finished, time used:  " + time_used + "(s)";
 	elif (type == "started"):
 		node_id = args["node_id"];
 		task = args["task"];
 
 		change_state(state_file_name, task, "pending");
-		str = "%s %s %s started\n" % (strftime, node_id, task)
-		fp.write(str);
+		fp.write(strftime + " " + node_id + " " + task + " started\n");
+		str = strftime + " " + node_id + " " + task + " started";
 	elif (type == "terminated"):
 		node_id = args["node_id"];
 		task = args["task"];
 
 		change_state(state_file_name, task, "terminated");
 	
-		str = "%s %s %s terminated\n" % (strftime, node_id, task)
-		fp.write(str);
-
+		fp.write(strftime + " " + node_id + " " + task + " terminated\n");
+		str = strftime + " " + node_id + " " + task + " terminated";
 	fp.close();
 	
 	return str;
+
+#update_state_file("state","20160727",start_time="20070913",is_init=True);
+#update_state_file("state","20160802");
+
+#auth = iplane.read_auth("auth","iplane");
+#end_time = iplane.get_latest_time_fromsite(auth[0],auth[1])
+#update_state_file("state",end_time,start_time="20060623",is_init=True);
---
+++ thesis_downloader_dist/manager/cron.py
@@ -0,0 +1,12 @@
+import datetime
+
+import caida
+
+start_time = "20070913"
+date = datetime.date.today().isoformat().replace('-','')
+#update_state_file("state","20160727",start_time="20070913",is_init=True);
+#update_state_file("state","20160802");
+
+#auth = iplane.read_auth("auth","iplane");
+#end_time = iplane.get_latest_time_fromsite(auth[0],auth[1])
+#update_state_file("state",end_time,start_time="20060623",is_init=True);
